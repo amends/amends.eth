3cf4e6ed93cc8c544691a1e5ce9a4f2c91b4da61
amends <amends@protonmail.com>
2021-08-15 17:15:53 -0700

    modify iframe

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,16 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { AspectRatio, Button, ButtonGroup } from "@chakra-ui/react"
 
 const Home: NextPage = () => {
     return (
-        <iframe src='https://opensea.io/amends?embed=true'
-                width='100%'
-                height='100%'
-                frameBorder='0'
-                allowFullScreen></iframe>
+        <AspectRatio ratio={16 / 9}>
+            <iframe
+            src="https://opensea.io/amends?embed=true"
+        
+            />
+        </AspectRatio>
     )
 }
-
 export default Home
